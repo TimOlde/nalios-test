1176beb2f32aa4bde3def979741142b0c001a863
TimOlde <tim.olde@gmail.com>
2026-04-23 11:01:58 +0200

    my sum

---
+++ 5_my_sum.py
@@ -0,0 +1,15 @@
+
+def my_sum_iterative(input: list[float]):
+    result = 0
+    for i in input:
+        result += i
+    return result
+
+def my_sum_recursive(input: list[float], agg = 0):
+    if len(input) == 0:
+        return agg
+    return my_sum_recursive(input[1:], agg + input[0])
+
+
+print(my_sum_iterative([1,2,3,4,5,6,7]))
+print(my_sum_recursive([1,2,3,4,5,6,7]))
